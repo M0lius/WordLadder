1f33a60faa437e9245813dd417485b3951710dd3
Maxwell Archibald <maxwellmarchibald@utexas.edu>
2016-03-01 13:46:32 -0600

    Dictionary: Basic Functionality
    The basic initialization of the dictionary's words have been
    "implemented". Still need to check for errors.

--- src/assignment4/Dictionary.java
+++ src/assignment4/Dictionary.java
@@ -5,5 +5,39 @@ public class Dictionary {
 	public Dictionary() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	public void Init(filename) {
+		
+		Dictionary dictionary = new Arraylist[String];
+		
+		try 
+		{
+			FileReader freader = new FileReader(filename);
+			BufferedReader reader = new BufferedReader(freader);
+			
+			i = 0;
+			for (String s = reader.readLine(); s != null; s = reader.readLine()) 
+			{
+				if (s.charAt(0) != '*'){
+					for (j = 0; j < 5; j++) {
+						Arraylist[i] += s.charAt(j);
+					}
+					i++;
+				}
+			}
+		} 
+		catch (FileNotFoundException e) 
+		{
+			System.err.println ("Error: File not found. Exiting...");
+			e.printStackTrace();
+			System.exit(-1);
+		} catch (IOException e) 
+		{
+			System.err.println ("Error: IO exception. Exiting...");
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		String word = new string();
+	}
 
 }
